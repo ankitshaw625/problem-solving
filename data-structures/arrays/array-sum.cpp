618b3ca9fca039870def303ea50fbc369bdbc4ad
Ankit Shaw <ankit.shaw0625@gmail.com>
2023-05-19 11:39:32 +0530

    Implement array sum

--- data-structures/arrays/array-sum.cpp
+++ data-structures/arrays/array-sum.cpp
@@ -18,8 +18,21 @@ int *get_array_input()
   return nums;
 }
 
+int sum_array(int nums[])
+{
+  int sum = 0;
+
+  for (int i = 0; i < n; i++)
+  {
+    sum += nums[i];
+  }
+
+  return sum;
+}
+
 int main()
 {
   int *nums = get_array_input();
+  cout << "Sum of the entered numbers: " << sum_array(nums) << endl;
   return 0;
 }
