a9a3f11a63d0fbf5b1a484cf984ae20d6d1eb79d
Ankit Shaw <ankit.shaw0625@gmail.com>
2023-05-30 20:35:28 +0530

    Implement sieve of eratosthenes

---
+++ data-structures/sieve-of-eratosthenes.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printPrimes(int limit)
+{
+  if (limit < 2)
+    return;
+
+  vector<bool> composites(limit + 1);
+  composites[0] = true;
+  composites[1] = true;
+
+  for (int i = 2; i <= limit / 2; i++)
+  {
+    if (composites[i])
+      continue;
+
+    for (int j = 2 * i; j <= limit; j += i)
+      composites[j] = true;
+  }
+
+  cout << "Primes upto " << limit << ": " << endl;
+  for (int i = 2; i <= limit; i++)
+  {
+    if (!composites[i])
+      cout << i << " ";
+  }
+}
+
+int main()
+{
+  printPrimes(2);
+  return 0;
+}
